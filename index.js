dd89ce2446135c660bf04e3f6064d90b32443910
Y1 <youssefabdo4005@gmail.com>
2024-06-10 13:45:56 +0300

    --done

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ app.use(compression());
 app.use(express.json());
 app.use(cors());
 
-app.use(cors({
-  origin: 'https://dern-support-server.vercel.app/*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
 app.get('/', (req, res) => {
   res.send('Dern Support Server!');
 });

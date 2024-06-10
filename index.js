9133b18f510c73ee57e2b9185923b64922d6d7a9
Y1 <youssefabdo4005@gmail.com>
2024-06-10 13:33:39 +0300

    --finish

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-  origin: '*', 
+  origin: 'https://dern-support-server.vercel.app/*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
